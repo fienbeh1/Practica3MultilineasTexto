b7dafb1d7348c7aad4639268205b3997b98440c8
Daniel Vazquez <fienbeh@gmail.com>
2023-11-29 13:46:54 -0600

    Agregar archivos de proyecto.

---
+++ Practica3MultilineasTexto.cpp
@@ -0,0 +1,45 @@
+// Practica3MultilineasTexto.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+int main()
+{
+    
+    std::cout << "3 Gacela de la terrible presencia, de Federico García Lorca\n";
+
+        std::cout << "Yo quiero que el agua se quede sin cauce.\n";
+        std::cout << "Yo quiero que el viento se quede sin valles.\n";
+
+        std::cout << "Quiero que la noche se quede sin ojos\n";
+        std::cout << "y mi corazón sin la flor del oro.\n";
+
+        std::cout << "Que los bueyes hablen con las grandes hojas\n";
+        std::cout << "y que la lombriz se muera de sombra.\n";
+
+        std::cout << "Que brillen los dientes de la calavera\n";
+        std::cout << "y los amarillos inunden la seda.\n";
+
+        std::cout << "Puedo ver el duelo de la noche herida\n";
+        std::cout << "luchando enroscada con el mediodía.\n";
+
+        std::cout << "Resisto un ocaso de verde veneno\n";
+        std::cout << "y los arcos rotos donde sufre el tiempo.\n";
+
+        std::cout << "Pero no me enseñes tu limpio desnudo\n";
+        std::cout << "como un negro cactus abierto en los juncos.\n";
+
+        std::cout << "Déjame en un ansia de oscuros planetas,\n";
+        std::cout << "¡pero no me enseñes tu cintura fresca!\n";
+
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
